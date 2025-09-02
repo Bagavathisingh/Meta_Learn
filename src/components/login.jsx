8bde156f533cc3d4edb5a40104af23f2ed8f4756
bagavathisingh <bugzx25@gmail.com>
2025-09-02 15:19:57 +0530

    login bug chages

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -26,23 +26,23 @@ export default function Login(){
             alert(error);
         }
     }
-    useEffect(()=>{
-        onAuthStateChanged(auth,(user)=>{
-        if(user){
-            Navigate("home",{replace:true});
-        }
-    });
-        },[]);
+    // useEffect(()=>{
+    //     onAuthStateChanged(auth,(user)=>{
+    //     if(user){
+    //         Navigate("home",{replace:true});
+    //     }
+    // });
+    //     },[]);
     return(
         <>
         <div id="loginBack" className=" bg-blue-100 h-240 w-full shirnk-1 flex flex-col justify-center items-center">
-            <div className="drop-shadow h-130 w-100 mt-20 md:-mt-10 p-10 md:p-0 md:h-100 md:w-150 mb-20 flex justify-center items-center rounded-lg">
+            <div className="drop-shadow h-130 w-100 mt-10 md:-mt-10 p-10 md:p-0 md:h-100 md:w-150 mb-20 flex justify-center items-center rounded-lg">
                 <h1 className="absolute mb-85 mr-40 text-6xl -m-10 md:mt-10 md:-ml-30 font-serif cursor-default">Login</h1>
                 <form className=" h-30 flex mb-10 flex-col justify-between gap-10 md:p-1">
-                    <div className="h-10 w-80 border-b-2">
+                    <div className="h-10 md:w-100 w-90 border-b-2">
                     <input required onChange={(e)=>setusername(e.target.value)} className="p-2 h-10 w-full placeholder:text-black outline-none focus:placeholder-transparent transition duration-300 ease-in-out" type="mail" name="username" placeholder="Email"/>
                     </div>
-                    <div className="h-10 w-80 border-b-2">
+                    <div className="h-10 md:w-100 w-90 border-b-2">
                         <div className=" absolute ml-69 mt-[7px]">
                             <button onClick={passShows} className="invert">
                                 {passShow?<img src={VisibleOn} />:<img src={VisibleOff}/>}
@@ -51,7 +51,7 @@ export default function Login(){
                     <input required onChange={(e)=>setpassword(e.target.value)} className="p-2 h-10 w-full placeholder:text-black outline-none focus:placeholder-transparent transition duration-300 ease-in-out" type={passShow?"password":"text"} name="password" placeholder="Password"/>
                     </div>
                     <div className="h-full w-full flex justify-center">
-                        <button onClick={loginhandle} className=" cursor-pointer py-1 rounded-lg  w-full h-full text-xl bg-blue-600 text-white hover:bg-blue-500 flex justify-center items-center font-serif">
+                        <button onClick={loginhandle} className=" cursor-pointer py-1 rounded-lg  w-full h-full text-xl bg-orange-600 text-white hover:bg-orange-500 flex justify-center items-center font-serif">
                             Login
                         </button>
                     </div>
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -30,14 +30,14 @@ export default function Register(){
     return(
         <>
         <div id="loginBack" className=" bg-blue-100 h-230 w-full shirnk-1 flex flex-col justify-center items-center">
-            <div className="drop-shadow rounded-lg h-130 w-100 p-10 md:h-100 md:w-150 mb-20 flex justify-center items-center">
-                <h1 className="absolute mb-85 mr-40 text-6xl -m-10 md:mt-10 md:-ml-30 font-serif cursor-default">SignUp</h1>
-                <form onSubmit={SignUpHandle} className=" h-30 flex mb-10 flex-col justify-between gap-10 p-1">
-                    <div className="h-10 w-100 border-b-2">
+            <div className="drop-shadow h-130 w-100 mt-10 md:-mt-10 p-10 md:p-0 md:h-100 md:w-150 mb-20 flex justify-center items-center rounded-lg">
+                <h1 className="absolute mb-85 mr-40 text-6xl -m-10 md:mt-10 ml-10 md:-ml-30 font-serif cursor-default">SignUp</h1>
+                <form onSubmit={SignUpHandle} className=" h-30 flex mb-10 flex-col justify-between items-center gap-10 md:p-1">
+                    <div className="h-10 md:w-100 w-90 border-b-2">
                     <input required onChange={(e)=>setusername(e.target.value)} className="p-2 h-10 w-full placeholder:text-black outline-none focus:placeholder-transparent transition duration-300 ease-in-out" type="mail" name="username" id="username" placeholder="Email"/>
                     </div>
-                    <div  className="h-10 w-100 border-b-2">
-                        <div className=" absolute ml-92 mt-[7px]">
+                    <div  className="h-10 md:w-100 w-90 border-b-2">
+                        <div className=" absolute ml-79 mt-[7px]">
                             <button onClick={passShows} className="invert">
                                 {passShow?<img src={VisibleOn} />:<img src={VisibleOff}/>}
                             </button>
@@ -45,7 +45,7 @@ export default function Register(){
                     <input required onChange={(e)=>setpassword(e.target.value)} className="p-2 h-10 w-full placeholder:text-black outline-none focus:placeholder-transparent transition duration-300 ease-in-out" type={passShow?"password":"text"} name="password" id="password" placeholder="Password"/>
                     </div>
                     <div className="h-full w-full flex justify-center">
-                        <button type="submit" className="cursor-pointer py-1 rounded-lg  w-full h-full text-xl bg-blue-600 text-white hover:bg-blue-500 flex justify-center items-center font-serif">
+                        <button type="submit" className="cursor-pointer py-1 rounded-lg  w-full h-full text-xl bg-orange-600 text-white hover:bg-orange-500 flex justify-center items-center font-serif">
                             Sign-Up
                         </button>
                     </div>
